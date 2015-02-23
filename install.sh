b7f28d62d53f26fa8bc7038802b401301ab2a687
Kai Davenport <kaiyadavenport@gmail.com>
2015-02-23 18:50:18 +0000

    systemd files for both services

--- install.sh
+++ install.sh
@@ -3,39 +3,52 @@
 #export ARPANET2_ADDRESS=${ARPANET2_ADDRESS:=/var/run/arpanet2.address}
 
 cmd-flocker-zfs-agent() {
-	cat << EOF >> /etc/rc.d/rc.local
+	local IP="$1";
+	local CONTROLIP="$2";
 
+	if [[ -z "$CONTROLIP" ]]; then
+		CONTROLIP="127.0.0.1";
+	fi
+
+	cat << EOF >> /etc/systemd/system/zfs-agent
+[Unit]
+Description=Flocker ZFS Agent
+
+[Service]
+TimeoutStartSec=0
+ExecStart=sudo flocker-zfs agent $IP $CONTROLIP
 EOF
 }
 
 cmd-flocker-control-service() {
-	cat << EOF >> /etc/rc.d/rc.local
+	cat << EOF >> /etc/systemd/system/flocker-control
+[Unit]
+Description=Flocker Control Service
 
+[Service]
+TimeoutStartSec=0
+ExecStart=sudo flocker-control -p 80
 EOF
 }
 
-cmd-setup-file() {
-	cat << EOF > /etc/rc.d/rc.local
-#!/bin/bash
-
-EOF
-}
 usage() {
 cat <<EOF
 Usage:
 install.sh flocker-zfs-agent
-install.sh flocker-control
+install.sh flocker-control-service
 install.sh help
 EOF
 	exit 1
 }
 
 main() {
-	cmd-setup-file
-
-	if [[ "$1" == "node1" ]]; then
-
-	fi
+	case "$1" in
+	flocker-zfs-agent)  			shift; cmd-flocker-zfs-agent $@;;
+	flocker-control-service)	shift; cmd-flocker-control-service $@;;
+	*)                  			usage $@;;
+	esac
 }
+
+# 
 
 main "$@"
